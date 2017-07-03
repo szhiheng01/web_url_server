81aa536206223b273aac5d6f7e01eb4ed54f0a59
宋志恒 <zhihengsong2@creditease.cn>
2017-07-03 22:57:20 +0800

    判断参数为空

--- src/main/java/com/creditease/mybaties/service/Impl/OperationUrlServiceImpl.java
+++ src/main/java/com/creditease/mybaties/service/Impl/OperationUrlServiceImpl.java
@@ -4,6 +4,7 @@ import com.creditease.mybaties.Utils.ResoultInfo;
 import com.creditease.mybaties.dao.UrlInfoMapper;
 import com.creditease.mybaties.model.UrlInfoModel;
 import com.creditease.mybaties.service.OperationUrlService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,29 +21,36 @@ public class OperationUrlServiceImpl implements OperationUrlService {
     @Autowired
     private UrlInfoMapper urlInfoMapper;
 
-    @Override
     public ResoultInfo insertUrl(UrlInfoModel urlInfoModel) {
 
-        logger.info("urlInfoModel = " + urlInfoModel);
-        ResoultInfo resoultInfo = new ResoultInfo();
-        if (urlInfoModel == null) {
+        ResoultInfo resoultInfo = null;
+        try {
+            resoultInfo = new ResoultInfo();
+            if (urlInfoModel == null || StringUtils.isBlank(urlInfoModel.getProjectName()) || StringUtils.isBlank(urlInfoModel.getUrlName())) {
+                resoultInfo.setCode(0);
+                resoultInfo.setStatus("false");
+                resoultInfo.setMessage("插入失败～～");
+                logger.info("urlInfoModel = " + urlInfoModel);
+            }
+            int insertCount = urlInfoMapper.insert(urlInfoModel);
+            logger.info("insertCount = " + insertCount);
+            if (insertCount == 0) {
+                resoultInfo.setCode(0);
+                resoultInfo.setStatus("false");
+                resoultInfo.setMessage("插入失败～～");
+            } else {
+                resoultInfo.setCode(1);
+                resoultInfo.setStatus("true");
+                resoultInfo.setMessage("插入成功～～");
+            }
+            logger.info("resoultInfo = " + resoultInfo);
+
+
+        } catch (Exception e) {
             resoultInfo.setCode(0);
             resoultInfo.setStatus("false");
             resoultInfo.setMessage("插入失败～～");
         }
-        int insertCount = urlInfoMapper.insert(urlInfoModel);
-        logger.info("insertCount = " + insertCount);
-        if (insertCount == 0) {
-            resoultInfo.setCode(insertCount);
-            resoultInfo.setStatus("true");
-            resoultInfo.setMessage("插入失败～～");
-        } else {
-            resoultInfo.setCode(insertCount);
-            resoultInfo.setStatus("true");
-            resoultInfo.setMessage("插入成功～～");
-        }
-
-        logger.info("resoultInfo = " + resoultInfo);
         return resoultInfo;
     }
 }
