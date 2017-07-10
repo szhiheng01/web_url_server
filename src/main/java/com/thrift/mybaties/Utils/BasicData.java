b14892cac341d4058058f2bb2c7cdc6e68462e12
宋志恒 <zhihengsong2@creditease.cn>
2017-07-10 14:46:16 +0800

    修改密码为*

---
+++ src/main/java/com/thrift/mybaties/Utils/BasicData.java
@@ -0,0 +1,23 @@
+package com.thrift.mybaties.Utils;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+/**
+ * Created by songzhiheng on 2017/7/10.
+ */
+public class BasicData extends BasicDataSource {
+
+    public BasicData() {
+        // TODO Auto-generated constructor stub
+        super();
+    }
+
+    @Override
+    public void setPassword(String password){
+        try{
+//            this.password =
+        }catch(Exception e){
+
+        }
+    }
+}
---
+++ src/test/java/com/jmeter/once/TestJmeter.java
@@ -0,0 +1,7 @@
+package com.jmeter.once;
+
+/**
+ * Created by songzhiheng on 2017/7/9.
+ */
+public class TestJmeter {
+}
